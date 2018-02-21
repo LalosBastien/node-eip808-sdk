d964a986f38369dfc9ebc56df0089b591f75041b
Bastien Lalos <bastien.lalos@gmail.com>
2018-02-21 15:17:49 +0100

    delete migrations_tmp

--- migrations_tmp/2_deploy_tokens.js
+++
@@ -1,5 +0,0 @@
-const token = artifacts.require('./BTU.sol');
-
-module.exports = (deployer) => {
-  deployer.deploy(token);
-};
--- migrations_tmp/3_deploy_factory.js
+++
@@ -1,5 +0,0 @@
-const factory = artifacts.require('./BTUFactory.sol');
-
-module.exports = (deployer) => {
-  deployer.deploy(factory);
-};
