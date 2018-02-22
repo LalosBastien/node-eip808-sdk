18a9f2c63234ff270cfb71a93b8c13cdea85cc53
Steven BOEHM <boehm_s@etna-alternance.net>
2018-02-22 18:00:26 +0100

    tmp solution

--- cjs/index.js
+++ cjs/index.js
@@ -5,6 +5,5 @@ module.exports = {
     Contract: require('./contractFactory'),
     RES: require('./contracts/RES'),
     accountList: require('./utils/accountList'),
-    ipfs: require('./utils/ipfs'),
-    RESABI: require('./../ressources/build/contracts/RES.json')
+    ipfs: require('./utils/ipfs')
 };
