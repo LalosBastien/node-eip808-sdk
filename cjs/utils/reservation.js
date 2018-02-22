e7ce90785426df19fc44f65b046b9a522ec8946c
Steven BOEHM <boehm_s@etna-alternance.net>
2018-02-22 18:14:37 +0100

    WIP

--- cjs/utils/reservation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// RESAddress
-// depositAmount
-var makeReservation = function makeReservation(bookerAddress, availabilityID) {
-    // 2 steps : approve (BTU) & transfer (RES)
-
-};
--- cjs/utils/web3Conf.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Web3 = require('web3');
-var web3 = new Web3();
-var HTTP_PROVIDER = process.env.HTTP_PROVIDER || 'http://localhost:9545';
-
-web3.setProvider(new Web3.providers.HttpProvider(HTTP_PROVIDER));
-
-module.exports = web3;
