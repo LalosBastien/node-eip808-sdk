d3e9efd59f81ac50103fb27580a0af693b6385e1
Sylvain Lasjunies <megasyl20@gmail.com>
2018-02-22 16:21:12 +0100

    pre build es5

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# node-eip808-sdk
-Npm module that allow request on the Ethereum BlockChain with BTU protocol (EIP808)
+#SDK Documentation
--- SDK/ressources/migrations/2_deploy_BTU.js
+++
@@ -1,5 +0,0 @@
-const token = artifacts.require('./BTU.sol');
-
-module.exports = (deployer) => {
-    deployer.deploy(token, 42000000, 'Booking Token Unit', 3, 'BTU');
-};
