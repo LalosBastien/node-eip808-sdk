f818c33628cbf94a22d869b708a1dcec32ef031a
Sylvain Lasjunies <megasyl20@gmail.com>
2018-02-22 16:21:12 +0100

    pre build es5

--- SDK/ressources/migrations/1_initial_migration.js
+++
@@ -1,5 +0,0 @@
-const migrations = artifacts.require('./Migrations.sol');
-
-module.exports = (deployer) => {
-  deployer.deploy(migrations);
-};
--- SDK/ressources/migrations/3_deploy_RES.js
+++
@@ -1,5 +0,0 @@
-const res = artifacts.require('./RES.sol');
-
-module.exports = (deployer) => {
-  deployer.deploy(res);
-};
